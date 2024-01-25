930facb97bbf3c9298e5d5d9b23a2e01640e8db4
jessdobs <jess.dobs@gmail.com>
2024-01-25 14:41:19 -0500

    set up constructor for Game and Phrase classes add event listeners to start button and keyboard create array of phrase objects remove overlay when user clicks start add functionality to pick a random phrase from the phrases array

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,56 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+
+class Game {
+    constructor(missed, phrases, activePhrase) {
+        this.missed = missed;
+        this.phrases = phrases;
+        this.activePhrase = activePhrase;
+    }
+
+    startGame() {
+        //hides the start screen overlay
+        overlay.style.display = 'none';
+        this.activePhrase = this.getRandomPhrase();
+        console.log(this.activePhrase);
+        //calls getRandomPhrase()
+        //sets activePhrase property wtih the chosen phrase
+        //this.activePhrase = getRandomPhrase();
+        //add phrase to the board by calling addPhraseToDisplay() method on the active phrase property
+    }
+
+  
+    getRandomPhrase() {
+        //getRandomPhrase() randomly retrieves one of the phrases stored in the phrases array and returns it
+        const randomIndex = Math.floor(Math.random() * (phrases.length + 1));
+        return phrases[randomIndex];
+    }
+
+    
+    handleInteraction(letter) {
+        console.log(letter);
+        //controls most of the game logic:
+        //checks to see if the button clicked by the player matches a letter in the phrase and directs the game based on whether the player's guess is correct or incorrect
+        // - disable selected letter's onscreen keyboard button
+        // - if the phrase does not include the letter, add the wrong class to the selected letter's keyboard button and call the removeLive() method
+        // - if the phrase includes the guessed letter, add the chosen class to the selected letter's keyboard button and call the showMatchedLetter() method on the phrase; then call checkForWin()
+        // - if the player has won the game, also call the gameOver() method
+    }
+
+    removeLife() {
+        //remove a life from the scoreboard by replacing one of the liveHeart.png images with a lostHeart.png image and increments the missed property. if the player has five missed guesses (i.e. they are out of lives), then end the game by calling the gameOver() method
+    }
+
+    
+    checkForWin() {
+        //checks to see if the player has revealed all of the letters in the active phrase
+    }
+
+    
+    gameOver() {
+        //this method displays the original start screen overlay and depending on the outcome of the game, updates the overlay h1 element with a friendly win or loss message and replaces the overlay's start class with either the win or lose class
+        overlay.style.display = 'flex';    
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,58 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+const phraseStrings = [
+    'Once in a blue moon',
+    'Piece of cake',
+    'Raining cats and dogs',
+    'When pigs fly',
+    'Poke the bear',
+    'Blow off steam',
+    'Throw in the towel',
+    'A shot in the dark',
+    'Let the cat out of the bag',
+    'Back against a wall',
+    'Saved by the bell',
+    'Break a leg',
+    'Barking up the wrong tree',
+    'Spill the beans',
+    'Under the weather'
+];
+
+const phrases = phraseStrings.map(phrase => phrase = new Phrase(phrase));
+
+const overlay = document.querySelector('#overlay');
+const start_button = document.querySelector('#btn__reset');
+const keyboard = document.querySelector('#qwerty');
+
+// add a click event listener to the start game button which creates a new Game object and calls the startGame method
+start_button.addEventListener('click', () => {
+    game = new Game(0, phrases, null);
+    game.startGame();
+    return game;
+});
+
+
+// add click event listeners to each of the onscreen keyboard buttons so that clicking a button calls the handleInteraction() method on the Game object. Event delegation can also be used in order to avoid having to add an event listener to each individual keyboard button. clicking the space between and around the onscreen keyboard buttons SHOULD NOT result in the handleInteraction() method being called
+keyboard.addEventListener('click', e => {
+    if (e.target.className === 'key') {
+        const selectedLetter = e.target.innerText;
+        game.handleInteraction(selectedLetter);
+    }
+});
+
+
+// after a game is completed, the gameboard needs to be reset so that clicking the "start game" button will successfully load a new game -- to reset the game, complete the following steps:
+// - remove all li elements from the phrase ul element
+// - enable all of the onscreen keyboard buttons and update each to use the key css class and not use the chosen or wrong css classes
+// - reset all of the heart images (i.e. the player's lives) in the scoreboard at the bottom of the gameboard to display the liveHeart.png
+
+// // FINISHING THE PROJECT
+// - code comments: it's best practice for development code to be well commented. replace provided comments iwth your own to briefly describe your code
+// - code readability: readability is second only to functionality. double-check your code to ensure the spacing and indentation are consistent
+// - cross-browser consistency: to pass, your project only needs to work in chrome, but it's common for developers to test their projects in multiple browsers to know how they will perform out in the wild
+// - QA testing: this is the keystone step in the development process:
+// -- -- open and run your app
+// -- -- open the Chrome DevTools console
+// -- -- pretend to be a user and test all aspects of functionality and every possible state of the app, while monitoring the console for bugs and resolving any that arise
+
