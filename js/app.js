985482fc45e2620b0e4b13483148fa48ad15500b
jessdobs <jess.dobs@gmail.com>
2024-01-26 14:54:16 -0500

    pull out original strings for phrases into their own file thinking I could eventually pull in any array of phrases to use with game

--- js/app.js
+++ js/app.js
@@ -1,25 +1,6 @@
 /**
- * Variables relating to the phrases used in the game
- * @type {array} @name phraseStrings array of strings 
  * @type {array} @name phrases array of @type {Phrase} instances
  */
-const phraseStrings = [
-    'Once in a blue moon',
-    'Piece of cake',
-    'Raining cats and dogs',
-    'When pigs fly',
-    'Poke the bear',
-    'Blow off steam',
-    'Throw in the towel',
-    'A shot in the dark',
-    'Let the cat out of the bag',
-    'Back against a wall',
-    'Saved by the bell',
-    'Break a leg',
-    'Barking up the wrong tree',
-    'Spill the beans',
-    'Under the weather'
-];
 const phrases = phraseStrings.map(phrase => new Phrase(phrase));
 
 /**
@@ -65,11 +46,8 @@ keyboard.addEventListener('click', e => {
 });
 
 // // FINISHING THE PROJECT
-// - code comments: it's best practice for development code to be well commented. replace provided comments iwth your own to briefly describe your code
-// - code readability: readability is second only to functionality. double-check your code to ensure the spacing and indentation are consistent
+
+
 // - cross-browser consistency: to pass, your project only needs to work in chrome, but it's common for developers to test their projects in multiple browsers to know how they will perform out in the wild
-// - QA testing: this is the keystone step in the development process:
-// -- -- open and run your app
-// -- -- open the Chrome DevTools console
-// -- -- pretend to be a user and test all aspects of functionality and every possible state of the app, while monitoring the console for bugs and resolving any that arise
+
 
---
+++ js/data.js
@@ -0,0 +1,20 @@
+/**
+ * @type {array} @name phraseStrings array of strings 
+ */
+const phraseStrings = [
+    'Once in a blue moon',
+    'Piece of cake',
+    'Raining cats and dogs',
+    'When pigs fly',
+    'Poke the bear',
+    'Blow off steam',
+    'Throw in the towel',
+    'A shot in the dark',
+    'Let the cat out of the bag',
+    'Back against a wall',
+    'Saved by the bell',
+    'Break a leg',
+    'Barking up the wrong tree',
+    'Spill the beans',
+    'Under the weather'
+];
